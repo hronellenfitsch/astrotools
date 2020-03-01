8ddfaa3d063727c9728ad9952d1cbccbac2ce8b5
Henrik Ronellenfitsch <henrikr@mit.edu>
2020-03-01 13:18:20 -0500

    aim.py uses proper tmp dir now and does not re-implement functions from capture.py

--- src/aim.py
+++ src/aim.py
@@ -5,40 +5,12 @@ import time
 import os
 import os.path
 import subprocess
-from glob import glob
-
+import tempfile
 import argparse
 
-import gphoto2 as gp
+from glob import glob
 
-def take_exposure(i, dir):
-    print(f"Exposure {i+1}...")
-    camera = gp.Camera()
-    camera.init()
-    file_path = camera.capture(gp.GP_CAPTURE_IMAGE)
-
-    _, ext = os.path.splitext(file_path.name)
-    target = os.path.join(dir, f'{i}{ext}')
-
-    camera_file = camera.file_get(
-        file_path.folder, file_path.name, gp.GP_FILE_TYPE_NORMAL)
-
-    camera_file.save(target)
-    camera.exit()
-
-    return target
-
-def plate_solve(fname):
-    path, ext = os.path.splitext(fname)
-
-    wcsfile = path + ".wcs"
-
-    dirn = os.path.dirname(fname)
-    print("Plate-solving...")
-
-    return subprocess.Popen(["solve-field", "-z2", "-Nnone", "-L3", "-H7",
-    "--match", "none", "--rdls", "none", "--corr", "none", "--solved", "none", "--index-xyls", "none",
-    "--overwrite", "--no-plots", f"-D{dirn}", fname])
+from capture import take_exposure, plate_solve
 
 def annotate(fname):
     path, ext = os.path.splitext(fname)
@@ -49,33 +21,28 @@ def annotate(fname):
     return subprocess.Popen(["plotann.py",
     wcsfile, fname, "--grid-size=1", "--scale=0.2", outfile])
 
-def cleanup(dir):
-    subprocess.run(["rm"] + glob(os.path.join(dir, "*")))
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--dir", default="aim_tmp/", help="Directory for temporary files")
     parser.add_argument("--aim-file", default="", type=str, help="Aim this file instead of taking exposure")
+    parser.add_argument("--L", default=3.0, type=float, help="approximate length-wise image scale in degrees")
+    parser.add_argument("--H", default=7.0, type=float, help="approximate height-wise image scale in degrees")
 
     args = parser.parse_args()
 
-    print("Cleaning working directory")
-    os.makedirs(args.dir, exist_ok=True)
-    cleanup(args.dir)
+    with tempfile.TemporaryDirectory() as tmpdir:
+        if args.aim_file == "":
+            fname = take_exposure(0, tmpdir)
+        else:
+            fname = args.aim_file
 
-    if args.aim_file == "":
-        fname = take_exposure(0, args.dir)
-    else:
-        fname = args.aim_file
+        process = plate_solve(fname, L=args.L, H=args.H)
+        process.communicate()
 
-    process = plate_solve(fname)
-    process.communicate()
+        process = annotate(fname)
+        process.communicate()
 
-    process = annotate(fname)
-    process.communicate()
-
-    # display image
-    path, ext = os.path.splitext(fname)
-    aimed_fname = path + "_ann.jpg"
-    subprocess.run(["xdg-open", aimed_fname])
+        # display image
+        path, ext = os.path.splitext(fname)
+        aimed_fname = path + "_ann.jpg"
+        subprocess.run(["xdg-open", aimed_fname])
